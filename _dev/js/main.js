07cb758dc9831282841cb3a20e95dc3ce4efc3ad
Eugen <evgeniy.sayenko@gmail.com>
2017-03-25 15:10:12 +0200

    Mobile страницы готовы, меню начал

--- _dev/js/main.js
+++ _dev/js/main.js
@@ -6,10 +6,52 @@ $('.logo__slider').bxSlider({
 });
 
 $('.ticker__slider').bxSlider({
-    minSlides: 4,
+    minSlides: 1,
     maxSlides: 12,
     slideWidth: 170,
     slideMargin: 10,
     ticker: true,
     speed: 200000
 });
+
+/// mobile menu
+
+var dropdownItem = document.querySelector(".mobile__menu-item-services");
+var dropdownList = document.querySelector(".dropdown__list");
+
+dropdownItem.addEventListener("click", function (event) {
+    event.preventDefault();
+    dropdownList.classList.toggle("show");
+});
+
+dropdownItem.addEventListener("tap", function (event) {
+    event.preventDefault();
+    dropdownList.classList.toggle("show");
+});
+
+
+var openNav = document.querySelector(".mobile__nav-button");
+var closeNav = document.querySelector(".close");
+var navMob = document.querySelector(".mobile__menu");
+
+openNav.addEventListener("click", function (event) {
+    event.preventDefault();
+    navMob.classList.add("show");
+})
+
+closeNav.addEventListener("click", function (event) {
+    event.preventDefault();
+    navMob.classList.remove("show");
+})
+
+openNav.addEventListener("tap", function (event) {
+    event.preventDefault();
+    navMob.classList.add("show");
+})
+
+closeNav.addEventListener("tap", function (event) {
+    event.preventDefault();
+    navMob.classList.remove("show");
+})
+
+
